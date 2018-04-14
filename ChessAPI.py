61c653cda5e7e0511304eac8993c69e9824db8a2
Warren Sunada-Wong <wsunada-wong20@newarka.edu>
2018-04-14 12:26:50 -0400

    Chess website

--- ChessAPI.py
+++ ChessAPI.py
@@ -6,16 +6,26 @@ Created on Sat Apr 14 09:12:57 2018
 @author: Warren
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
 import requests
 site = Flask(__name__)
 api_url = 'https://api.chess.com/pub/player/The20thDuck'
 @site.route('/')
 def home():
-    api_data = requests.get(api_url).json()
-    stat_data = requests.get(api_url + '/stats').json()
-    return render_template('Home.html', datas = api_data, stats = stat_data)
+    return render_template('Home.html')
+
+@site.route('/player', methods = ['POST', 'GET'])
+def player():
+    if request.method == 'POST':
+        playername = request.form
+        player_url = 'https://api.chess.com/pub/player/' + playername['Player']
+        player_data = requests.get(player_url).json()  
+        if 'player_id' in player_data.keys():
+            stat_data = requests.get(player_url + '/stats').json()
+            return render_template('Player.html', player = playername, datas = player_data, stats = stat_data)
+        else:
+            return render_template('Nonexistent.html')
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5000))
 	site.run(host="0.0.0.0", port=port, threaded=True)
